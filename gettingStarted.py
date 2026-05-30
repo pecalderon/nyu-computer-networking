1e42706dce283a5ded8e4b752e0e0757022d428d
pecalderon <70454990+pecalderon@users.noreply.github.com>
2026-05-30 02:13:33 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -22,7 +22,7 @@ def welcome_assignment_answers(question):
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = int("4")
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = int("2")
+        answer = int("3")
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
